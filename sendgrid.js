766b418abb744ca7ebee98915650b8f580bb420f
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-04 16:31:24 -0800

    add sendgrid logic

--- sendgrid.js
+++ sendgrid.js
@@ -1,50 +1,26 @@
+require('dotenv').config();
 const client = require('@sendgrid/mail');
 
 client.setApiKey(process.env.SENDGRID_API_KEY);
-console.log('api key', process.env.SENDGRID_API_KEY);
-const message = {
-  personalizations: [
-    {
-      to: [
-        {
-          email: 'kirstenhammondrvn@gmail.com',
-          name: 'John Doe'
-        }
-      ]
-    },
-    {
-      from: {
-        email: 'kirstenhammondrvn@gmail.com',
-        name: 'Example Sales Team'
-      },
-      to: [
-        {
-          email: 'kirstenhammondrvn@gmail.com',
-          name: 'Janice Doe'
-        }
-      ]
-    }
-  ],
-  from: {
-    email: 'kirstenhammondrvn@gmail.com',
-    name: 'kirsten rvn'
-  },
-  replyTo: {
-    email: 'kirstenhammondrvn@gmail.com',
-    name: 'kirsten returned'
-  },
-  subject: 'Your Example Order Confirmation',
-  content: [
-    {
-      type: 'text/html',
-      value: '<p>Hello from Twilio SendGrid!</p><p>Sending with the email service trusted by developers and marketers for <strong>time-savings</strong>, <strong>scalability</strong>, and <strong>delivery expertise</strong>.</p><p>%open-track%</p>'
-    }
-  ]
-};
+//const apiKey = `${process.env.SENDGRID_API_KEY}`;
+//console.log('api key', apiKey);
 
+const msg = {
+  to: 'kirsty.hammond@hotmail.co.uk', // Change to your recipient
+  from: 'kirstenhammondrvn@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+const sendEmailToClient = () => {
 client
-  .send(message)
+  .send(msg)
   .then(() => console.log('Mail sent successfully'))
   .catch(error => {
-    console.error('error', error);
-  });
+    console.error('error', JSON.stringify(error, null, 4));
+    //console.log(error.body);
+  })
+};
+
+sendEmailToClient();
